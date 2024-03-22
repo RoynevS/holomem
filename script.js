2815aa3b80715e3398b23778de623c2935cc5166
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-03-22 20:58:31 +0100

    Create random gameboard following game rules
    gameboard consists of 24 pairs. The pairs are arranged randomly on the
    gameboard.

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const testValues = [
+  "a", 
+  "b", 
+  "c", 
+  "d", 
+  "e", 
+  "f", 
+  "g", 
+  "h", 
+  "i", 
+  "j", 
+  "k", 
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "ö",
+  "ä",
+  "ü",
+];
+
+
 function createPlayer(name) {
   let score = 0;
   const getName = () => name;
@@ -12,18 +45,42 @@ function createPlayer(name) {
 }
 
 function gameboard() {
+  let counter = 0;
   const rows = 6;
   const columns = 8;
   const board = [];
 
+  const getRandomArray = () => {
+    const gameArray = []
+
+    while (gameArray.length < 24) {
+      const randomIdol = testValues[Math.floor(Math.random() * testValues.length)];
+      if (!gameArray.includes(randomIdol)) gameArray.push(randomIdol);
+    }
+
+    const fullArray = gameArray.concat([...gameArray]);
+
+    for (let i = fullArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [fullArray[i], fullArray[j]] = [fullArray[j], fullArray[i]];
+    }
+
+    return fullArray;
+  };
+
+  const gameArray = getRandomArray();
+
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
-      board[i].push(card());
+      board[i].push(card(gameArray[counter]));
+      counter++;
     }
   }
 
+
   const getBoard = () => board;
+
 
   const printBoard = () => {
     const boardWithValues = board.map(row => row.map(card => card.getValue()));
@@ -33,13 +90,12 @@ function gameboard() {
   return { getBoard, printBoard };
 }
 
-function card() {
-  let value = 0;
 
+function card(value) {
   const getValue = () => value;
 
   return { getValue };
 }
 
 const newBoard = gameboard();
-console.log(newBoard.printBoard());
+newBoard.printBoard();
